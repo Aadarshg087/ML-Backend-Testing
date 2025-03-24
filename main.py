66779d36eaa192b8cb7088a06deb43b88fcfdbe7
aadarsh <aadarshg087@gmail.com>
2025-03-24 15:00:27 +0530

    Code

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+import numpy as np
+import tensorflow.lite as tflite
+import io
+
+app = FastAPI()
+
+# Load the TensorFlow Lite model
+interpreter = tflite.Interpreter(model_path="mobilenet_leaf_model.tflite")
+interpreter.allocate_tensors()
+
+# Get input/output details
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+def preprocess_image(image_bytes):
+    image = Image.open(io.BytesIO(image_bytes))
+    image = image.resize((224, 224))  # Adjust based on model requirements
+    image = np.array(image, dtype=np.float32) / 255.0  # Normalize
+    image = np.expand_dims(image, axis=0)  # Add batch dimension
+    return image
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    image_tensor = preprocess_image(image_bytes)
+
+    # Run inference
+    interpreter.set_tensor(input_details[0]['index'], image_tensor)
+    interpreter.invoke()
+    output = interpreter.get_tensor(output_details[0]['index'])
+
+    predicted_class = np.argmax(output)  # Get the highest probability class
+
+    return {"prediction": int(predicted_class)}
